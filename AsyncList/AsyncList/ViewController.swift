4d1917a6d0452dcfcff76d8a51324951cf15b31b
wannabewize <wannabewize@gmail.com>
2017-02-17 02:11:59 +0900

    Step1 : 동기식으로 목록 출력

---
+++ AsyncList/AsyncList/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  AsyncList
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // https://github.com/wannabewize/AsyncListExample/blob/master/images/l.png?raw=true
+    let baseURL = "https://github.com/wannabewize/AsyncListExample/blob/master/images/"
+    let data = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p","q","r","s","t","u","v","w","x","y","z"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath)
+        
+        let character = data[indexPath.row]
+        cell.textLabel?.text = character
+        
+        // 이미지 경로
+        let urlStr = "\(baseURL)\(character).png?raw=true"
+        if let url = URL(string: urlStr),
+            let imageData = try? Data(contentsOf: url) {
+            
+            let image = UIImage(data: imageData)
+            cell.imageView?.image = image
+        }
+        
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
