3bd66d3a5f4cfa48582c5a1f0a5a2c5022ebf96f
wannabewize <wannabewize@gmail.com>
2017-02-18 05:44:09 +0900

    비동기 적용

--- AsyncList/AsyncList/ViewController.swift
+++ AsyncList/AsyncList/ViewController.swift
@@ -8,6 +8,10 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // https://github.com/wannabewize/AsyncListExample/blob/master/images/l.png?raw=true
+    var queue : OperationQueue!
+    
+    var cache = [URL : UIImage]()
+    
     let baseURL = "https://github.com/wannabewize/AsyncListExample/blob/master/images/"
     let data = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     
@@ -24,14 +28,35 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // 이미지 경로
         let urlStr = "\(baseURL)\(character).png?raw=true"
         let url = URL(string: urlStr)!
-        let imageData = try! Data(contentsOf: url)
-        let image = UIImage(data: imageData)
-        cell.imageView?.image = image
+        
+        if let image = cache[url] {
+            cell.imageView?.image = image
+        }
+        else {
+            queue.addOperation {
+                // 이미지 다운로드
+                let imageData = try! Data(contentsOf: url)
+                
+                // 이미지 변환
+                let image = UIImage(data: imageData)
+                
+                // 캐쉬에 저장
+                self.cache[url] = image
+                
+                OperationQueue.main.addOperation {
+                    cell.imageView?.image = image
+                }
+            }
+        }
+        
         return cell
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 5
     }    
 }
