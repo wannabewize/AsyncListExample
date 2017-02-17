ec1a04302527897c791b081605ad0fcb1ff3e2e1
wannabewize <wannabewize@gmail.com>
2017-02-18 01:24:22 +0900

    이미지 추가

--- AsyncList/AsyncList/ViewController.swift
+++ AsyncList/AsyncList/ViewController.swift
@@ -9,7 +9,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     // https://github.com/wannabewize/AsyncListExample/blob/master/images/l.png?raw=true
     let baseURL = "https://github.com/wannabewize/AsyncListExample/blob/master/images/"
-    let data = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p","q","r","s","t","u","v","w","x","y","z"]
+    let data = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
