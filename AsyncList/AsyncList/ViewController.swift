80fe8cbbf432ed774d84081930c660919ebe8611
wannabewize <wannabewize@gmail.com>
2017-02-18 05:10:22 +0900

    Revert "OperationQueue 적용"
    This reverts commit be8d1fee9295f6cfc28ea101318dd3b497d58309.

--- AsyncList/AsyncList/ViewController.swift
+++ AsyncList/AsyncList/ViewController.swift
@@ -9,9 +9,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     // https://github.com/wannabewize/AsyncListExample/blob/master/images/l.png?raw=true
     let baseURL = "https://github.com/wannabewize/AsyncListExample/blob/master/images/"
-    let data = ["a","b","c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"]
-    
-    var queue : OperationQueue!
+    let data = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
@@ -25,16 +23,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         // 이미지 경로
         let urlStr = "\(baseURL)\(character).png?raw=true"
-        let url = URL(string: urlStr)!
-        
-        queue.addOperation {
-            let imageData = try! Data(contentsOf: url)
+        if let url = URL(string: urlStr),
+            let imageData = try? Data(contentsOf: url) {
             
             let image = UIImage(data: imageData)
-            
-            OperationQueue.main.addOperation {
-                cell.imageView?.image = image
-            }
+            cell.imageView?.image = image
         }
         
         return cell
@@ -43,9 +36,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        queue = OperationQueue()
-        queue.maxConcurrentOperationCount = 5
     }
     
-}//https://github.com/wannabewize/AsyncListExample/blob/step1/images/0.png?raw=true
+}
